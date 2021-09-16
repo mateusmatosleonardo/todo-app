94a5d6330840e51164e29aba4bb7c98200060649
Mateus Leonardo <m4teusmatos@gmail.com>
2021-09-16 16:16:59 -0300

    criando primeio arquivo de resposta

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res)=>{
+    res.send('Carregado')
+})
+
 app.listen(8080, ()=>{
     console.log('server no ar')
 })
