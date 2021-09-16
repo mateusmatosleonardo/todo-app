cc38c6c1daebfbfb8819a6e1232576e519a4dc61
Mateus Leonardo <m4teusmatos@gmail.com>
2021-09-16 16:39:17 -0300

    criando dependências

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
 app.get('/', (req, res)=>{
-    res.send('Carregado')
+    
+    res.render('index', {})
+
 })
 
 app.listen(8080, ()=>{
