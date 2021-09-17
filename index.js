642c0fc497221bc38021987f5cad3ee358a726e1
Mateus Leonardo <m4teusmatos@gmail.com>
2021-09-17 15:06:17 -0300

    finalizando

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser');
 
 const app = express();
+
+app.use( bodyParser.json() )
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
-let tasks = ['estudar estrutura de dados', 'arrumar o quarto']
+let tasks = []
 
 app.post('/', (req, res)=>{
-    console.log(req.body.task)
+    tasks.push(req.body.task)
+    res.render('index', {tasksList:tasks})
 })
 
 app.get('/', (req, res)=>{
